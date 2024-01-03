67fd070392ca37aada6eb07820b9acb2867c6577
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-03 14:59:14 +0400

    Update operatorfor2.java

--- operatorfor2.java
+++ operatorfor2.java
@@ -1,10 +1,10 @@
-public class operatorfor2{
-public static void main(String[] arg){
-for (int i=1; i<=100; i++){
-if ((i==5 || i==7 || i==11) || (i>=20 && i<=30) || (i>=45 && i<=55)){
-continue;
-}
-System.out.println(i);
-}
-}
+public class operatorfor2 {
+  public static void main(String[] arg) {
+    for (int i = 1; i <= 100; i++) {
+      if ((i == 5 || i == 7 || i == 11) || (i >= 20 && i <= 30) || (i >= 45 && i <= 55)) {
+        continue;
+      }
+      System.out.println(i);
+    }
+  }
 }
